774040aef5beef7d322886acd51f392f52a52855
Stanlee <stanz.ferigh@gmail.com>
2021-01-08 23:57:34 +1100

    initial components & scss added

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './components/Header'
+import Form from './components/Form'
+import Anime from './components/Anime'
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+      <Form />
+      <Anime />
+    </div>
+  )
+}
+
+export default App
